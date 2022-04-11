35aa2103f380414d3034d6224b5b2b6266b834dd
Rodrigo William <rwps1234@gmail.com>
2022-04-11 11:03:02 -0300

    subindo atividade GET API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/usuario', function (req, res) {
-  res.send('usuario')
+  res.send('Rota ativada com GET e recurso usuario valores de usuario devem ser retornados')
 })
 app.get('/endereco', function (req, res) {
     res.send('Rua documentação')
